ff8f5b64a835c18e437e3b468f62781dfe31983e
a-burlakov <fln@mail.ru>
2023-03-06 20:10:07 +0500

    refactor: some little refactoring.

--- readme_generate.py
+++ readme_generate.py
@@ -3,7 +3,7 @@ import requests
 
 def generate_readme():
     """
-    Generates the README.md file by setting up parameters (looks like "$TEST")
+    Generates the README.md file by setting up parameters
     in "readme_template.md".
     """
 
@@ -11,6 +11,7 @@ def generate_readme():
     url_origin = "http://aaburlakov.ru"  # protocol + host
     api_recent_posts = "/api/v1/recentarticles"
     response = requests.get(f"{url_origin}{api_recent_posts}")
+
     if response.status_code != 200:
         return
 
@@ -18,16 +19,17 @@ def generate_readme():
     recent_posts = response.json()
     recent_posts_md_texts = []
     for post in recent_posts:
-        recent_posts_md_texts.append(
-            f"* [{post['title']}]({url_origin + post['get_absolute_url']})")
+        md_link_line = f"* [{post['title']}]({url_origin + post['get_absolute_url']})"
+        recent_posts_md_texts.append(md_link_line)
     recent_posts_text = "\n".join(recent_posts_md_texts)
 
     # Generating README.md based on the template.
     with open("readme_template.md", "r", encoding="utf-8") as readme_template:
         md_template = readme_template.read()
     with open("README.md", "w") as readme:
-        readme_md_text = md_template.format(RECENT_POSTS=recent_posts_text,
-                                            URL_ORIGIN=url_origin)
+        readme_md_text = md_template.format(
+            RECENT_POSTS=recent_posts_text, URL_ORIGIN=url_origin
+        )
         readme.write(readme_md_text)
 
 
