ce9833660fdc8e561ca7d6e430a76281b4453cb3
a-burlakov <fln@mail.ru>
2023-03-06 16:53:27 +0500

    feat: trying to set GitHub Action.

---
+++ generate_readme.py
@@ -0,0 +1,30 @@
+import json
+import logging.handlers
+import os
+
+import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
+    # logger.info("Token not available!")
+    # raise
+
+if __name__ == '__main__':
+    resp = requests.get('http://aaburlakov.ru/api/v1/recentarticles')
+    result = json.dumps(resp.json(), ensure_ascii=False)
+    logger.info(result)
+    print(result)
